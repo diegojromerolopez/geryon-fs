fb1ad95b5b8570af41f692e0ee34b4a4bb99b79e
diegojromerolopez <diegojromerolopez@gmail.com>
2021-05-25 23:00:19 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
 import os
 from setuptools import setup, find_packages
 
-path = os.path.abspath(__file__)
+root_dir_path = os.path.dirname(os.path.abspath(__file__))
 
 try:
     import pypandoc
     long_description = pypandoc.convert('README.md', 'rst')
 except(IOError, ImportError):
-    long_description = open(os.path.join(path, 'README.md')).read()
+    long_description = open(os.path.join(root_dir_path, 'README.md')).read()
 
-data_files = []
-for dirpath, dirnames, filenames in os.walk(path):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        continue
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-with open(os.path.join(path, "requirements.txt")) as requirements_file:
+with open(os.path.join(root_dir_path, "requirements.txt")) as requirements_file:
     requirements = requirements_file.readlines()
 
 setup(
@@ -38,7 +28,8 @@ setup(
     license="BSD 2-clause",
     keywords="database mongo fuse driver",
     url='https://github.com/diegojromerolopez/geryon-fs',
-    packages=find_packages(path),
-    data_files=data_files,
+    packages=find_packages(root_dir_path),
+    data_files=[],
     include_package_data=True,
+    scripts=['bin/mount.py']
 )
